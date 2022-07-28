80ae5151bc3103ec053dbc1e6fc04c2472fa44a1
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-07-28 15:05:52 -0700

    complete Models and develop Stylist Index + Create Vs+Cs

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -4,5 +4,14 @@ namespace HairSalon.Models
 {
   public class Stylist
   {
+    public int StylistId { get; set; }
+    public string StylistName { get; set; }
+    public string StylistSpecialty { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
+
+    public Stylist()
+    {
+      this.Clients = new HashSet<Client>();
+    }
   }
 }
