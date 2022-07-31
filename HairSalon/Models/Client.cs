c6c554cacc5543636d2c68ca24d7fe01005c1074
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-07-30 22:39:56 -0700

    refactor all views

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace HairSalon.Models
 {
   public class Client
   {
     public int ClientId { get; set; }
+    [Display(Name = "Client Name")]
     public string ClientName { get; set; }
     public int StylistId { get; set; }
     public virtual Stylist Stylist { get; set; }
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -1,11 +1,14 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace HairSalon.Models
 {
   public class Stylist
   {
     public int StylistId { get; set; }
+    [Display(Name = "Stylist Name")]
     public string StylistName { get; set; }
+    [Display(Name = "Stylist Specialty")]
     public string StylistSpecialty { get; set; }
     public virtual ICollection<Client> Clients { get; set; }
 
@@ -14,6 +17,6 @@ namespace HairSalon.Models
       this.Clients = new HashSet<Client>();
     }
 
-    
+
   }
 }
